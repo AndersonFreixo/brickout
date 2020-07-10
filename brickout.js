b8498163d6097f5fea6d1b5116af4f9f11cbcf5d
AndersonFreixo <48022430+AndersonFreixo@users.noreply.github.com>
2020-07-09 21:31:11 -0300

    Implemented start button and start screen

--- brickout.js
+++ brickout.js
@@ -2,14 +2,47 @@
 document.addEventListener("mousemove", mouseMoveHandler, false);
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("click", clickHandler, false);
 
+let events = {activeLoop: "start",
+              rightPressed: false,
+              leftPressed: false,
+              relativeX: 0,
+              relativeY:0,
+              mouseClick: false};
+
+/*Image loading*/
+let wallpaper = new Image();
+wallpaper.src = `resources/img/wallpaper.png`;
+let logoImg = new Image();
+logoImg.src = "resources/img/logo.png";
+
+/*Button configuration*/
+
+let startButtonImg = new Image();
+startButtonImg.src = "resources/img/start_button.png";
+let startButtonOverImg = new Image();
+startButtonOverImg.src = "resources/img/start_button_over.png";
+
+let startButton = {mouseOver: startButtonOverImg,
+                   mouseOut: startButtonImg,
+                  x: SCREEN_WIDTH/2 - startButtonImg.width/2,
+                  y: 300,
+                  active: "start"};
+
+/*Audio loading*/
+let audioManager = new AudioManager();
+audioManager.loadToSwitcher("paddleHit", "resources/audio/paddle_hit.wav", 4);
+audioManager.loadToSwitcher("brickHit", "resources/audio/brick_hit.wav", 4);
+
+/*Canvas setup*/
 /*This is the DIV where the two canvas are put*/
 let screenDiv = document.getElementById("screen");
 
 /*headerCanvas shows game info*/
 let headerCanvas = document.createElement("canvas");
 let headerCtx = headerCanvas.getContext("2d");
-headerCanvas.style.cursor = "None";
+//headerCanvas.style.cursor = "None";
 headerCanvas.height = HEADER_HEIGHT;
 headerCanvas.width = SCREEN_WIDTH;
 
@@ -18,21 +51,13 @@ screenDiv.appendChild(headerCanvas);
 /*"canvas" is where the game graphics are rendered*/
 let canvas = document.createElement("canvas");
 let ctx = canvas.getContext("2d");
-canvas.style.cursor = "None";
+//canvas.style.cursor = "None";
 canvas.height = GAME_HEIGHT;
 canvas.width = SCREEN_WIDTH;
 
 screenDiv.appendChild(canvas);
 
-let wallpaper = new Image();
-wallpaper.src = `resources/img/wallpaper.png`;
-
-
-
 /*Entities*/
-
-let audioManager = new AudioManager();
-
 let paddle = new Paddle(x       = (canvas.width - PAD_WIDTH)/2,
                         y       = canvas.height - PAD_HEIGHT,
                         height  = PAD_HEIGHT,
@@ -50,9 +75,59 @@ levelManager.initLevel();
 
 let gameState = new GameState;
 
-let events = {rightPressed: false, leftPressed: false, relativeX: 0};
 
-function run(){
+function isCoordInside(x1, y1, x2, y2, width, height){
+  if (x1 >= x2 && x1 <= x2+width &&
+    y1 >= y2 && y1 <= y2+height){
+      return true;
+  }
+  else return false;
+}
+
+
+
+function startLoop(){
+      ctx.drawImage(logoImg, 0, 0, 320, 460);
+
+      /*A black strip in the bottom of the screen*/
+      ctx.beginPath();
+      ctx.fillStyle = "#000000";
+      ctx.rect(0, GAME_HEIGHT-20, SCREEN_WIDTH, 20);
+      ctx.fill();
+      ctx.closePath();
+
+      /*Blackout the header canvas*/
+      headerCtx.beginPath();
+      headerCtx.fillStyle = "#000000";
+      headerCtx.rect(0, 0, SCREEN_WIDTH, 20);
+      headerCtx.fill();
+      headerCtx.closePath();
+
+      if (isCoordInside(events.relativeX,
+                        events.relativeY,
+                        startButton.x,
+                        startButton.y,
+                        startButton.mouseOver.width,
+                        startButton.mouseOver.height)){
+
+
+        ctx.drawImage(startButton.mouseOver, startButton.x, startButton.y);
+        canvas.style.cursor = "Pointer";
+      }
+      else{
+        ctx.drawImage(startButton.mouseOut, startButton.x, startButton.y);
+        canvas.style.cursor = "Default";
+      }
+      if (events.activeLoop == "main"){
+        canvas.style.cursor = "None";
+        requestAnimationFrame(gameLoop);
+      }
+      else{
+        requestAnimationFrame(startLoop);
+      }
+}
+
+function gameLoop(){
     if(canvas.getContext){
         /*updates*/
         paddle.update(events);
@@ -139,14 +214,15 @@ function run(){
     else{
         document.write("Oooops, no canvas!");
     }
-    requestAnimationFrame(run);
+    requestAnimationFrame(gameLoop);
 }
 
-run();
+startLoop();
 
 /*Event handlers*/
 function mouseMoveHandler(e){
   events.relativeX = e.clientX - canvas.offsetLeft;
+  events.relativeY = e.clientY - canvas.offsetTop;
 }
 
 function keyDownHandler(e){
@@ -165,4 +241,14 @@ function keyUpHandler(e){
     else if (e.key == "Left" || e.key == "ArrowLeft"){
         events.leftPressed = false;
     }
+}
+
+function clickHandler(e){
+  let x = e.clientX - canvas.offsetLeft;
+  let y = e.clientY - canvas.offsetTop;
+  if (events.activeLoop = "start" &&
+  isCoordInside(x, y, startButton.x, startButton.y,
+                startButton.mouseOut.width, startButton.mouseOut.height)){
+      events.activeLoop = "main";
+  }
 }
