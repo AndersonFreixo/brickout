a73d9e08ee387b087d7355e4cbade9455fb83c26
AndersonFreixo <48022430+AndersonFreixo@users.noreply.github.com>
2020-07-12 21:01:25 -0300

    Add files via upload

--- brickout.js
+++ brickout.js
@@ -25,23 +25,22 @@ let gameState = new GameState;
 let eHandler = new EventHandler(canvas);
 let levelManager = new LevelManager();
 
-levelManager.initLevel();
-
 /*Audio setup*/
 let audioManager = new AudioManager();
 audioManager.loadToSwitcher("paddleHit", "resources/audio/paddle_hit.wav", 4);
 audioManager.loadToSwitcher("brickHit", "resources/audio/brick_hit.wav", 4);
 
-
 /*Image setup*/
 let interface = new InterfaceManager();
 interface.addImage("wallpaper", "resources/img/wallpaper.png");
-interface.addImage("logo", "resources/img/logo.png");
-interface.addImage("startOut", "resources/img/start_button.png");
-interface.addImage("startOver", "resources/img/start_button_over.png");
+interface.addImage("logo", "resources/img/logo-gradient.png");
+interface.addImage("startOut", "resources/img/start-button.png");
+interface.addImage("startOver", "resources/img/start-button-over.png");
 interface.addImage("paddle", "resources/img/paddle.png");
-interface.addImage("gamePause", "resources/img/game_pause.png");
-
+interface.addImage("gamePause", "resources/img/game-pause.png");
+interface.addImage("gameOver", "resources/img/game-over.png");
+interface.addImage("playAgainOut", "resources/img/play-again-button.png");
+interface.addImage("playAgainOver", "resources/img/play-again-button-over.png");
 
 /*Button configuration*/
 interface.addButton("startButton",
@@ -51,9 +50,22 @@ interface.addButton("startButton",
                     300,
                     "start",
                     function(){
-                      eHandler.events.activeLoop = "main";
-                      canvas.style.cursor = "None"});
+                      eHandler.events.activeLoop = "main";});
 
+interface.addButton("playAgainButton",
+                    interface.img["playAgainOut"],
+                    interface.img["playAgainOver"],
+                    SCREEN_WIDTH/2 - interface.img["playAgainOut"].width/2,
+                    300,
+                    "gameOver",
+                    function(){
+                      eHandler.events.activeLoop = "main";
+                      lastTimestamp = 0;
+                      levelManager.reset();
+                      levelManager.initLevel();
+                      gameState.reset();
+                      ball.toStartPosition();
+                    });
 /*Entities*/
 let paddle = new Paddle(x       = (canvas.width - PAD_WIDTH)/2,
                         y       = canvas.height - PAD_HEIGHT,
@@ -66,20 +78,34 @@ let ball = new Ball(x       = canvas.width/2,
                     radius  = BALL_RAD,
                     color   = `rgb(230, 0, 5)`);
 
+let lastTimestamp = 0;
+
+levelManager.initLevel();
 
 /*Run the game*/
-run();
+requestAnimationFrame(run);
 
 /*Main loop*/
 function run(timestamp){
+
   if (eHandler.events.activeLoop == "start"){
+    canvas.style.cursor = "Default";
     startLoop();
   }
+
   else if (eHandler.events.activeLoop == "main"){
+    canvas.style.cursor = "None";
     if (eHandler.events.pause == false){
+      /*Measures elapsed time in game loop*/
+      if (lastTimestamp == 0) lastTimestamp = timestamp;
+      else{
+        gameState.elapsed += timestamp - lastTimestamp;
+        lastTimestamp = timestamp;
+      }
       gameLoop();
     }
     else{
+        lastTimestamp = 0;
         let pauseImg = interface.img["gamePause"];
         ctx.drawImage(pauseImg, 0, (canvas.height-pauseImg.height)/2);
     }
@@ -88,6 +114,7 @@ function run(timestamp){
 
   }
   else if (eHandler.events.activeLoop == "gameOver"){
+    gameOverLoop();
 
   }
   requestAnimationFrame(run);
@@ -119,7 +146,7 @@ function startLoop(){
       }
 }
 
-function gameLoop(timestamp){
+function gameLoop(){
 
       /*updates*/
       paddle.update(eHandler.events);
@@ -147,7 +174,6 @@ function gameLoop(timestamp){
             ball.changeDirHorizontal();
           }
 
-
           if (levelManager.isLevelComplete()){
             if(!levelManager.nextLevel()){
               alert("YOU WIN, CONGRATULATIONS!");
@@ -160,32 +186,19 @@ function gameLoop(timestamp){
           }
       }
 
-      /*ball detection*/
-      let col = ball.checkWallCollision(canvas).toUpperCase();
-  //    if(col == "RIGHT" || col == "LEFT" || col == "TOP"){
-  //      audioManager.playSound("wallHit");
-  //    }
-      if (col == "RIGHT" || col == "LEFT"){
-        ball.changeDirHorizontal();
-      }
-      else if (col == "TOP"){
-        ball.changeDirVertical();
-      }
-      else if (col == "BOTTOM"){
+      if (ball.isTouchingWall(canvas) == "bottom"){
         if (gameState.lives >= 1){
           gameState.lives--;
-          ball.x = canvas.width/2;
-          ball.y = canvas.height -30;
-          ball.dx = 0.5;
-          ball.dy = -0.5;
-          paddle.x = (canvas.width-paddle.width)/2;
+          ball.toStartPosition();
+        //  paddle.x = (canvas.width-paddle.width)/2;
+        //  eHandler.events.relativeX = paddle.x;
         }
         else{
-          alert("Game Over!!!");
-          location.reload();
+          eHandler.events.activeLoop = "gameOver";
+          gameState.reset();
         }
       }
-      let p = paddle.hasTouched(ball.getRectangle());
+      let p = paddle.isTouching(ball.getRectangle());
       if (p  != -1 && ball.dy > 0){
           console.log(p);
           audioManager.playSound("paddleHit");
@@ -193,4 +206,16 @@ function gameLoop(timestamp){
       }
 
 
+}
+
+function gameOverLoop(){
+  canvas.style.cursor = "Default";
+  ctx.drawImage(interface.img["gameOver"], 0, 0, 320, 460);
+  interface.renderButtons("gameOver", ctx, canvas, eHandler.events.relativeX,
+                     eHandler.events.relativeY);
+
+  if (eHandler.events.mouseClick == true){
+      eHandler.events.mouseClick = false;
+      interface.processClick("gameOver", eHandler.events.relativeX, eHandler.events.relativeY);
+  }
 }
